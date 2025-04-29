fd433508f91bf992337bda762e6ace0b3324baae
JPR <puruem1215@naver.com>
2025-04-29 17:17:31 +0900

    VO수정: 줄간격

--- src/JAVAManage/Stu2VO.java
+++ src/JAVAManage/Stu2VO.java
@@ -1,51 +1,51 @@
 package JAVAManage;
 
 public class Stu2VO {
-
-	   private String name;
-	   private int korS;
-	   private int engS;
-	   private int matS;
-	   
-	   public Stu2VO() {}
-	   
-	   
-
-	   public int getKorS() {
-	      return korS;
-	   }
-	   public int getEngS() {
-	      return engS;
-	   }
-
-
-	   public void setEngS(int engS) {
-	      this.engS = engS;
-	   }
-	   
-	   
-	   public class Stu2Builder {
-		      private String name;
-		      private int korS;
-		      private int engS;
-		      private int matS;
-		      
-		      public Stu2Builder setName(String name) {
-		         this.name = name;
-		         return this;
-		      }
-		      public Stu2Builder setKorS(int korS) {
-		         this.korS = korS;
-		         return this;
-		      }
-		      public Stu2Builder setEngS(int engS) {
-		         this.engS = engS;
-		         return this;
-		      }
-		      public Stu2Builder setMatS(int matS) {
-		         this.matS = matS;
-		         return this;
-		      }
-		      
-	   }	   
+	
+	private String name;
+	private int korS;
+	private int engS;
+	private int matS;
+	
+	public Stu2VO() {}
+	
+	
+	
+	public int getKorS() {
+	   return korS;
+	}
+	public int getEngS() {
+	   return engS;
+	}
+	
+	
+	public void setEngS(int engS) {
+	   this.engS = engS;
+	}
+	
+	
+	public class Stu2Builder {
+		private String name;
+		private int korS;
+		private int engS;
+		private int matS;
+		
+		public Stu2Builder setName(String name) {
+		   this.name = name;
+		   return this;
+		}
+		public Stu2Builder setKorS(int korS) {
+		   this.korS = korS;
+		   return this;
+		}
+		public Stu2Builder setEngS(int engS) {
+		   this.engS = engS;
+		   return this;
+		}
+		public Stu2Builder setMatS(int matS) {
+		   this.matS = matS;
+		   return this;
+		}
+		
+	}
 }
