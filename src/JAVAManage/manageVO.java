1efaa56782e03ffc6ea210224f25326c88ffb6be
JPR <puruem1215@naver.com>
2025-04-29 16:48:59 +0900

    VO추가: 변수 지정, get()추가

--- src/JAVAManage/manageVO.java
+++ src/JAVAManage/manageVO.java
@@ -2,4 +2,18 @@ package JAVAManage;
 
 public class manageVO {
 
+	   private String name;
+	   private int korS;
+	   private int engS;
+	   private int matS;
+	   
+	   public manageVO() {}
+	   
+	   public int getKorS() {
+	      return korS;
+	   }
+	   public int getEngS() {
+	      return engS;
+	   }
+	
 }
