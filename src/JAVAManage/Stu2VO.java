7b61d1acd8d76688ffa0a67da99b4e34090b8ec2
KJS <wnsjtq781@naver.com>
2025-04-29 17:40:57 +0900

    중간점검

--- src/JAVAManage/Stu2VO.java
+++ src/JAVAManage/Stu2VO.java
@@ -2,32 +2,41 @@ package JAVAManage;
 
 public class Stu2VO {
 
-	   private String name;
-	   private int korS;
-	   private int engS;
-	   private int matS;
-	   
-	   public Stu2VO() {}
-	   
-	   public Stu2VO(Stu2Builder bd) {
-		      this.name = bd.name;
-		      this.korS = bd.korS;
-		      this.engS = bd.engS;
-		      this.matS = bd.matS;
-		   }
-	   
+	public Stu2VO(Stu2Builder bd){this.name=bd.name;this.korS=bd.korS;this.engS=bd.engS;this.matS=bd.matS;}
+	
+	private String name;
+	private int korS;
+	private int engS;
+	private int matS;
 
-	   public int getKorS() {
-	      return korS;
-	   }
-	   public int getEngS() {
-	      return engS;
-	   }
+	public Stu2VO() {
+	}
 
+	public Stu2VO(Stu2Builder bd) {
+		this.name = bd.name;
+		this.korS = bd.korS;
+		this.engS = bd.engS;
+		this.matS = bd.matS;
+	}
 
-	   public void setEngS(int engS) {
-	      this.engS = engS;
-	   }
-	   
-	   
+	public int getKorS() {
+		return korS;
+	}
+
+	public int getEngS() {
+		return engS;
+	}
+
+	public int getMatS() {
+		return matS;
+	}
+
+	public void setKorS(int korS) {
+		this.korS = korS;
+	}
+
+	public void setEngS(int engS) {
+		this.engS = engS;
+	}
+
 }
