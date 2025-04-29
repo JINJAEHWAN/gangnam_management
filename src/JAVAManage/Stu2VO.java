14a6dae8ecdd16ca9e34d7ede5dfa66e34056a6d
JPR <puruem1215@naver.com>
2025-04-29 16:56:05 +0900

    추가: Stu2VO

---
+++ src/JAVAManage/Stu2VO.java
@@ -0,0 +1,27 @@
+package JAVAManage;
+
+public class Stu2VO {
+
+	   private String name;
+	   private int korS;
+	   private int engS;
+	   private int matS;
+	   
+	   public Stu2VO() {}
+	   
+	   
+
+	   public int getKorS() {
+	      return korS;
+	   }
+	   public int getEngS() {
+	      return engS;
+	   }
+
+
+	   public void setEngS(int engS) {
+	      this.engS = engS;
+	   }
+	   
+	   
+}
